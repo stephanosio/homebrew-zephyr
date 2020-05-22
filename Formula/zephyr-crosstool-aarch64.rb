9b75a438ea636a06a0de32cfc454a86145b0e26e
Stephanos Ioannidis <root@stephanos.io>
2020-05-22 11:36:49 +0900

    Appease rubocop
    Signed-off-by: Stephanos Ioannidis <root@stephanos.io>

--- Formula/zephyr-crosstool-aarch64.rb
+++ Formula/zephyr-crosstool-aarch64.rb
@@ -1,53 +1,56 @@
-require 'formula'
+require "formula"
 
 class ZephyrCrosstoolAarch64 < Formula
-    desc "ARM AArch64 Cross Compiler Toolchain (aarch64-zephyr-elf)"
-    homepage 'https://github.com/stephanosio/zephyr-crosstool-ng'
-    version '1.24.0.2'
+  desc "ARM AArch64 Cross Compiler Toolchain"
+  homepage "https://github.com/stephanosio/zephyr-crosstool-ng"
+  url "https://github.com/stephanosio/zephyr-crosstool-ng/releases/download/zephyr-crosstool-ng-1.24.0.2/zephyr-crosstool-ng-1.24.0.2_macos-x86_64_aarch64-zephyr-elf.tar.gz"
+  version "1.24.0.2"
+  sha256 "352892f41958c9ed558884acac5452515a467141be0c572fab4ce47e970f8829"
+  revision 1
 
-    url 'https://github.com/stephanosio/zephyr-crosstool-ng/releases/download/zephyr-crosstool-ng-1.24.0.2/zephyr-crosstool-ng-1.24.0.2_macos-x86_64_aarch64-zephyr-elf.tar.gz'
-    sha256 '352892f41958c9ed558884acac5452515a467141be0c572fab4ce47e970f8829'
+  depends_on "python"
+  depends_on "xz"
 
-    def install
-        (prefix).install Dir["./*"]
-        Dir.glob(prefix/"aarch64-zephyr-elf/bin/*") { |file| bin.install_symlink file }
+  def install
+    prefix.install Dir["./*"]
+    Dir.glob(prefix/"aarch64-zephyr-elf/bin/*") { |file| bin.install_symlink file }
+  end
+
+  test do
+    expected = "(zephyr-crosstool-NG 1.24.0.2)"
+
+    [
+      "addr2line",
+      "ar",
+      "as",
+      "c++",
+      "c++filt",
+      "cc",
+      "cpp",
+      "elfedit",
+      "g++",
+      "gcc",
+      "gcc-9.2.0",
+      "gcc-ar",
+      "gcc-nm",
+      "gcc-ranlib",
+      "gcov",
+      "gcov-dump",
+      "gcov-tool",
+      "gdb",
+      "gprof",
+      "ld",
+      "ld.bfd",
+      "nm",
+      "objcopy",
+      "objdump",
+      "ranlib",
+      "readelf",
+      "size",
+      "strings",
+      "strip",
+    ].each do |cmd|
+      assert_match expected, shell_output("#{bin}/aarch64-zephyr-elf-#{cmd} --version")
     end
-
-    test do
-      expected = "(zephyr-crosstool-NG 1.24.0.2)"
-
-      [
-          "addr2line",
-          "ar",
-          "as",
-          "c++",
-          "c++filt",
-          "cc",
-          "cpp",
-          "elfedit",
-          "g++",
-          "gcc",
-          "gcc-9.2.0",
-          "gcc-ar",
-          "gcc-nm",
-          "gcc-ranlib",
-          "gcov",
-          "gcov-dump",
-          "gcov-tool",
-          "gdb",
-          "gprof",
-          "ld",
-          "ld.bfd",
-          "nm",
-          "objcopy",
-          "objdump",
-          "ranlib",
-          "readelf",
-          "size",
-          "strings",
-          "strip"
-      ].each { |cmd|
-          assert_match expected, shell_output("#{bin}/aarch64-zephyr-elf-#{cmd} --version")
-      }
-    end
+  end
 end
--- Formula/zephyr-crosstool-arm.rb
+++ Formula/zephyr-crosstool-arm.rb
@@ -1,53 +1,56 @@
-require 'formula'
+require "formula"
 
 class ZephyrCrosstoolArm < Formula
-    desc "ARM AArch32 (Cortex-A/R/M) Cross Compiler Toolchain (arm-zephyr-eabi)"
-    homepage 'https://github.com/stephanosio/zephyr-crosstool-ng'
-    version '1.24.0.2'
+  desc "ARM AArch32 (Cortex-A/R/M) Cross Compiler Toolchain"
+  homepage "https://github.com/stephanosio/zephyr-crosstool-ng"
+  url "https://github.com/stephanosio/zephyr-crosstool-ng/releases/download/zephyr-crosstool-ng-1.24.0.2/zephyr-crosstool-ng-1.24.0.2_macos-x86_64_arm-zephyr-eabi.tar.gz"
+  version "1.24.0.2"
+  sha256 "d4de6252c9766801ef8ac4832650b4affbb2a035adceb34c131d6a08a8246384"
+  revision 1
 
-    url 'https://github.com/stephanosio/zephyr-crosstool-ng/releases/download/zephyr-crosstool-ng-1.24.0.2/zephyr-crosstool-ng-1.24.0.2_macos-x86_64_arm-zephyr-eabi.tar.gz'
-    sha256 'd4de6252c9766801ef8ac4832650b4affbb2a035adceb34c131d6a08a8246384'
+  depends_on "python"
+  depends_on "xz"
 
-    def install
-        (prefix).install Dir["./*"]
-        Dir.glob(prefix/"arm-zephyr-eabi/bin/*") { |file| bin.install_symlink file }
+  def install
+    prefix.install Dir["./*"]
+    Dir.glob(prefix/"arm-zephyr-eabi/bin/*") { |file| bin.install_symlink file }
+  end
+
+  test do
+    expected = "(zephyr-crosstool-NG 1.24.0.2)"
+
+    [
+      "addr2line",
+      "ar",
+      "as",
+      "c++",
+      "c++filt",
+      "cc",
+      "cpp",
+      "elfedit",
+      "g++",
+      "gcc",
+      "gcc-9.2.0",
+      "gcc-ar",
+      "gcc-nm",
+      "gcc-ranlib",
+      "gcov",
+      "gcov-dump",
+      "gcov-tool",
+      "gdb",
+      "gprof",
+      "ld",
+      "ld.bfd",
+      "nm",
+      "objcopy",
+      "objdump",
+      "ranlib",
+      "readelf",
+      "size",
+      "strings",
+      "strip",
+    ].each do |cmd|
+      assert_match expected, shell_output("#{bin}/arm-zephyr-eabi-#{cmd} --version")
     end
-
-    test do
-      expected = "(zephyr-crosstool-NG 1.24.0.2)"
-
-      [
-          "addr2line",
-          "ar",
-          "as",
-          "c++",
-          "c++filt",
-          "cc",
-          "cpp",
-          "elfedit",
-          "g++",
-          "gcc",
-          "gcc-9.2.0",
-          "gcc-ar",
-          "gcc-nm",
-          "gcc-ranlib",
-          "gcov",
-          "gcov-dump",
-          "gcov-tool",
-          "gdb",
-          "gprof",
-          "ld",
-          "ld.bfd",
-          "nm",
-          "objcopy",
-          "objdump",
-          "ranlib",
-          "readelf",
-          "size",
-          "strings",
-          "strip"
-      ].each { |cmd|
-          assert_match expected, shell_output("#{bin}/arm-zephyr-eabi-#{cmd} --version")
-      }
-    end
+  end
 end
