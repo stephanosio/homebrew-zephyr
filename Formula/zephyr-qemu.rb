944032b8ff3c03d7c043009fd51e18e9b74de06f
Stephanos Ioannidis <root@stephanos.io>
2020-06-09 09:55:36 +0900

    Update zephyr-qemu formula to test ARC architecture emulator
    Signed-off-by: Stephanos Ioannidis <root@stephanos.io>

--- Formula/zephyr-qemu.rb
+++ Formula/zephyr-qemu.rb
@@ -6,7 +6,7 @@ class ZephyrQemu < Formula
   url "https://github.com/stephanosio/zephyr-qemu/releases/download/zephyr-qemu-v5.0.0.1/zephyr-qemu-v5.0.0.1_macos-x86_64.tar.gz"
   version "5.0.0.1"
   sha256 "917a769a1c9dd9e4889e3a388f20b36d0be088fbcdc97a7ba0a3207a23b513d0"
-  revision 1
+  revision 2
 
   depends_on "gettext"
   depends_on "glib"
@@ -23,7 +23,7 @@ class ZephyrQemu < Formula
   test do
     expected = "QEMU emulator"
 
-    %w[aarch64 arm i386 nios2 riscv32 riscv64 sparc x86_64 xtensa].each do |cmd|
+    %w[aarch64 arc arm i386 nios2 riscv32 riscv64 sparc x86_64 xtensa].each do |cmd|
       assert_match expected, shell_output("#{bin}/zephyr-qemu-system-#{cmd} --version")
     end
   end
